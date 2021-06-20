185982e270e41ca44dfb15c983133be767a5943d
Yaroslav <yaroslaff@gmail.com>
2021-06-21 01:52:22 +0700

    setup

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='other_access',
-    version='0.0.1',
+    version='0.0.2',
     packages=['other_access'],
     license='none',
     description='os.access() alternative which can check access for other users',
     long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     url='https://github.com/yaroslaff/other_access',
     author='Yaroslav Polyakov',
     author_email='yaroslaff@gmail.com'
